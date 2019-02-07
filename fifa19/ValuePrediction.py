ebc7fce9fd5d08c89a5bf8276da70718fb906552
shin <auseunghyuk@gmail.com>
2019-02-07 13:40:12 +0900

    revise variable and using pyplot

--- fifa19/ValuePrediction.py
+++ fifa19/ValuePrediction.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
+
 
 # 단위: €1K (1000유로)
 # Load File
@@ -10,7 +12,7 @@ data = np.loadtxt('data_modify.csv', delimiter=',', dtype=np.float32, encoding="
 
 # fit:내부적으로 normalization 에 필요한 값들을 계산해서 저장
 # fit_transform: 실제 각 값들이 normalization
-train_data_x = data[:9000, :1]  # [[age, overall, potential], ...]
+train_data_x = data[:9000, [1]]  # [[age, overall, potential], ...]
 train_data_y = data[:9000, [-1]]  # [[value], ...]
 '''
 train_data_x = MinMaxScaler().fit_transform(train_data_x)
@@ -18,12 +20,12 @@ y_scaler = MinMaxScaler()
 train_data_y = y_scaler.fit_transform(train_data_y)
 '''
 
-valid_data_x = data[9000:12000, :1]
+valid_data_x = data[9000:12000, [1]]
 valid_data_y = data[9000:12000, [-1]]
 #valid_data_x = MinMaxScaler().fit_transform(valid_data_x)
 #valid_data_y = MinMaxScaler().fit_transform(valid_data_y)
 
-test_data_x = data[12000:, :1]
+test_data_x = data[12000:, [1]]
 # just comparing
 test_data_y = data[12000:, [-1]]
 #test_data_x = MinMaxScaler().fit_transform(test_data_x)
@@ -40,7 +42,7 @@ hypothesis = tf.matmul(X, W) + b
 cost = tf.reduce_mean(tf.square(hypothesis - Y))
 
 # Training
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.0000001)
 train = optimizer.minimize(cost)
 '''
 prediction = tf.arg_max(hypothesis, 1)
@@ -58,9 +60,14 @@ for step in range(2001):
 
 # Test
 # print("Test ",sess.run(hypothesis, feed_dict={X:valid_data_x}))
-y = sess.run(hypothesis, feed_dict={X: [[52], [92]]})
+y = sess.run(hypothesis, feed_dict={X: [[80]]})
 print(y)
 #y_inverse = y_scaler.inverse_transform(y)
 #print(y_inverse)
 # print("Test ",MinMaxScaler().inverse_transform(sess.run(hypothesis, feed_dict={X: [[26, 92, 93]]})))
 # print("Test ", sess.run(hypothesis, feed_dict={X: [[39, 52, 52]]}))
+
+plt.plot(train_data_x, train_data_y, 'ro')
+plt.plot(train_data_x, sess.run(W)*train_data_x+sess.run(b))
+plt.legend()
+plt.show()
